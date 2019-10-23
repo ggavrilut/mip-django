5f6034bb2ea162cb638a9692d01999956c22d70e
Gavrilut Gabriel <gabriel.gavrilut@ekkode.com>
2019-10-23 22:37:03 +0300

    debugger for VSCode, Django, Docker

--- front/views.py
+++ front/views.py
@@ -9,6 +9,10 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'front/home.html'
+    def get(self, request):
+        print("hrere");
+
+        return render(request, self.template_name)
 
 class ContactView(TemplateView):
     template_name = 'front/contact.html'
